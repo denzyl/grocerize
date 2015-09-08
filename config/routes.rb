42163ca82610651ccd0bd5b87bde47c82661db60
Denzyl Balram <denzyl.balram@gmail.com>
2015-09-08 16:13:06 -0700

    Follow BCrypt directions (https://gist.github.com/thebucknerlife/10090014).

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   resources :users
 
-  root 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 end
