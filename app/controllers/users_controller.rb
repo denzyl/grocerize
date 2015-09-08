d1e7ad8e7f9e8c9e04afb8a120ccb9bc2c2faf11
Denzyl Balram <denzyl.balram@gmail.com>
2015-09-08 16:04:27 -0700

    Add brcypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
-  def new
-    @user = User.new
-  end
+  def new; end
 
   def edit; end
 
   def show; end
 
   def create
-    @user = User.new(user_params)
+    user = User.new(user_params)
 
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:notice] = "You are registered"
-      redirect_to root_path
+    if user.save
+      session[:user_id] = user.id
+      # flash[:notice] = "You are registered"
+      redirect_to '/'
     else
       render :new
     end
@@ -24,7 +22,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.require(:user).permit(:username, :password)
   end
 
   def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :items
   has_many :categories
 
-  has_secure_password validations: false
+  has_secure_password
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true, on: :create, length: {minimum: 5}
 end
