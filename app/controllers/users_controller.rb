52e2b61d3915e79a4a9612a8d452c310e49a8398
Denzyl Balram <denzyl.balram@gmail.com>
2015-09-08 15:43:12 -0700

    Add views for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
+    @user = User.new
   end
 
-  def edit
+  def edit; end
+
+  def show; end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "You are registered"
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def show
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password_digest)
   end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_many :purchases
+  has_many :items
+  has_many :categories
+
+  has_secure_password validations: false
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true, on: :create, length: {minimum: 5}
 end
