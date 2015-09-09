bf89692bcd5b656411a5f2a0f8d46dd5abcede7f
Denzyl Balram <denzyl.balram@gmail.com>
2015-09-08 18:56:03 -0700

    Small changes to BCrypt to make it work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:username])
+    user = User.find_by(params[:id])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def edit; end
 
   def show; end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       # flash[:notice] = "You are registered"
       redirect_to '/'
     else
